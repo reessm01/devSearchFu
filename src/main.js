9af9288fe506a911fd9ba39cccc860f8e08430e4
Scott Reese <scott_reese@pm.me>
2019-07-16 19:32:37 -0400

    basic and detailed inline form integrated, basic non-functioning

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import "./registerServiceWorker";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSearch)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
